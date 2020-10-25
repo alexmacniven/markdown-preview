82237cf404f6f72ebc7ba5017a54a8182f5e20ce
Alex Macniven <macniven.ap@gmail.com>
2020-10-25 17:03:47 +0000

    Adds App Logic and Styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import marked from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      preview : `# Macdown
+
+## A Markdown Editor by Alex Macniven
+
+> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber and Aaron Swartz.
+
+Hello, I'm Alex and I built this web application using React.
+It uses CSS Flexbox and is powered by [Marked](https://cdnjs.com/libraries/marked).
+
+You can read up on markdown syntax in [this article](https://daringfireball.net/projects/markdown/) over at Daring Fireball.
+
+## Contribute
+First, have the required tools;
+ - \`node\`
+ - \`npm\`
+
+Then you're ready to clone the project;
+\`\`\`bash
+git clone ...
+\`\`\`
+
+Launch a development build;
+\`\`\`bash
+npm start
+\`\`\``
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.formatMarkdown = this.formatMarkdown.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({preview : event.target.value})
+  }
+
+  formatMarkdown(){
+    const markdownText = marked(this.state.preview, {sanitize:true})
+    return {__html:markdownText}
+  }
+
+  render() {
+    return (
+      <section class="Container">
+          <div>
+            <textarea onChange={this.handleChange}>{this.state.preview}</textarea></div>
+          <div id="preview" dangerouslySetInnerHTML={this.formatMarkdown()}></div>
+      </section>
+    )
+  }
 }
 
 export default App;
